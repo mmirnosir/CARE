34e018372697e46033415dcc8c46a60e773d3e88
mmirnosir <mm22ahl@herts.ac.uk>
2024-04-23 21:42:15 +0100

    Refactored some of the functions. fixed errors.

--- src/cwk4/Tournament.java
+++ src/cwk4/Tournament.java
@@ -9,9 +9,8 @@ import java.io.*;
  * @version 
  */
 
-public class Tournament implements CARE
-{
-   
+public class Tournament implements CARE {
+
     private String vizier;
     private int treasury;
     private String filename;
@@ -19,232 +18,259 @@ public class Tournament implements CARE
 
 
 //**************** CARE ************************** 
-    /** Constructor requires the name of the vizier
+
+    /**
+     * Constructor requires the name of the vizier
+     *
      * @param viz the name of the vizier
-     */  
-    public Tournament(String viz)
-    {
+     */
+    public Tournament(String viz) {
         this.treasury = 1000;
 
-           setupChampions();
-           setupChallenges();
+        setupChampions();
+        setupChallenges();
     }
-    
-    /** Constructor requires the name of the vizier and the
+
+    /**
+     * Constructor requires the name of the vizier and the
      * name of the file storing challenges
-     * @param viz the name of the vizier
+     *
+     * @param viz      the name of the vizier
      * @param filename name of file storing challenges
-     */  
+     */
     public Tournament(String viz, String filename)  //Task 3.5
     {
         this.vizier = viz;
         this.filename = filename;
 
-       setupChampions();
-       readChallenges(filename);
+        setupChampions();
+        readChallenges(filename);
     }
-    
-    
-    /**Returns a String representation of the state of the game,
+
+
+    /**
+     * Returns a String representation of the state of the game,
      * including the name of the vizier, state of the treasury,
-     * whether defeated or not, and the champions currently in the 
+     * whether defeated or not, and the champions currently in the
      * team,(or, "No champions" if team is empty)
-     * 
+     *
      * @return a String representation of the state of the game,
      * including the name of the vizier, state of the treasury,
-     * whether defeated or not, and the champions currently in the 
+     * whether defeated or not, and the champions currently in the
      * team,(or, "No champions" if team is empty)
      **/
-    public String toString()
-    {
+    public String toString() {
         String s = "\nVizier: " + vizier + "\nTreasury: " + treasury + "\nDefeated: " + isDefeated() +
                 "\nChampions: " + getTeam();
-        
+
         return s;
     }
-    
-    
-    /** returns true if Treasury <=0 and the vizier's team has no 
-     * champions which can be retired. 
-     * @returns true if Treasury <=0 and the vizier's team has no 
-     * champions which can be retired. 
+
+    /**
+     * returns true if Treasury <=0 and the vizier's team has no
+     * champions which can be retired.
+     *
+     * @returns true if Treasury <=0 and the vizier's team has no
+     * champions which can be retired.
      */
-    public boolean isDefeated()
-    {
-        return false;
+    public boolean isDefeated() {
+        return this.treasury <= 0 && this.getReserve() == null;
     }
-    
-    /** returns the amount of money in the Treasury
+
+    /**
+     * returns the amount of money in the Treasury
+     *
      * @returns the amount of money in the Treasury
      */
-    public int getMoney()
-    {
+    public int getMoney() {
         return this.treasury;
     }
-    
-    
-    /**Returns a String representation of all champions in the reserves
+
+
+    /**
+     * Returns a String representation of all champions in the reserves
+     *
      * @return a String representation of all champions in the reserves
      **/
-    public String getReserve()
-    {   
-        String s = "************ Champions available in reserves********";
-        
+    public String getReserve() {
+        String s = "********** All champions *********";
+
+        if (championReserves.isEmpty()) {
+            return null;
+        }
+        else {
+            for (Champion champ : championReserves) {
+                s += "\n" + champ;
+            }
+        }
         return s;
     }
-    
-        
-    /** Returns details of the champion with the given name. 
+
+
+    /**
+     * Returns details of the champion with the given name.
      * Champion names are unique.
+     *
      * @return details of the champion with the given name
      **/
-    public String getChampionDetails(String nme)
-    {
-       
+    public String getChampionDetails(String nme) {
+//        if (championReserves.contains(nme)) {
+//            for (Champion champ : championReserves) {
+//                return champ.getDetails();
+//            }
+//        }
         return "\nNo such champion";
-    }    
-    
-    /** returns whether champion is in reserve
-    * @param nme champion's name
-    * @return true if champion in reserve, false otherwise
-    */
-    public boolean isInReserve(String nme)
-    {
-        return (false);
     }
- 
+
+    /**
+     * returns whether champion is in reserve
+     *
+     * @param nme champion's name
+     * @return true if champion in reserve, false otherwise
+     */
+    public boolean isInReserve(String nme) {
+        return championReserves.contains(nme);
+    }
+
     // ***************** Team champions ************************   
-     /** Allows a champion to be entered for the vizier's team, if there 
-     * is enough money in the Treasury for the entry fee.The champion's 
+
+    /**
+     * Allows a champion to be entered for the vizier's team, if there
+     * is enough money in the Treasury for the entry fee.The champion's
      * state is set to "active"
-     * 0 if champion is entered in the vizier's team, 
-     * 1 if champion is not in reserve, 
-     * 2 if not enough money in the treasury, 
-     * -1 if there is no such champion 
+     * 0 if champion is entered in the vizier's team,
+     * 1 if champion is not in reserve,
+     * 2 if not enough money in the treasury,
+     * -1 if there is no such champion
+     *
      * @param nme represents the name of the champion
      * @return as shown above
-     **/        
-    public int enterChampion(String nme)
-    {
-        
+     **/
+    public int enterChampion(String nme) {
+
         return -1;
     }
-        
-     /** Returns true if the champion with the name is in 
+
+    /**
+     * Returns true if the champion with the name is in
      * the vizier's team, false otherwise.
+     *
      * @param nme is the name of the champion
      * @return returns true if the champion with the name
      * is in the vizier's team, false otherwise.
      **/
-    public boolean isInViziersTeam(String nme)
-    {
+    public boolean isInViziersTeam(String nme) {
         return false;
     }
-    
-    /** Removes a champion from the team back to the reserves (if they are in the team)
+
+    /**
+     * Removes a champion from the team back to the reserves (if they are in the team)
      * Pre-condition: isChampion()
      * 0 - if champion is retired to reserves
      * 1 - if champion not retired because disqualified
      * 2 - if champion not retired because not in team
      * -1 - if no such champion
+     *
      * @param nme is the name of the champion
-     * @return as shown above 
+     * @return as shown above
      **/
-    public int retireChampion(String nme)
-    {
+    public int retireChampion(String nme) {
         return -1;
     }
-    
-    
-        
-    /**Returns a String representation of the champions in the vizier's team
+
+
+    /**
+     * Returns a String representation of the champions in the vizier's team
      * or the message "No champions entered"
+     *
      * @return a String representation of the champions in the vizier's team
      **/
-    public String getTeam()
-    {
+    public String getTeam() {
         String s = "************ Vizier's Team of champions********";
-        
+
 
         return s;
     }
-    
-     /**Returns a String representation of the disquakified champions in the vizier's team
+
+    /**
+     * Returns a String representation of the disquakified champions in the vizier's team
      * or the message "No disqualified champions "
+     *
      * @return a String representation of the disqualified champions in the vizier's team
      **/
-    public String getDisqualified()
-    {
+    public String getDisqualified() {
         String s = "************ Vizier's Disqualified champions********";
-        
-        
+
+
         return s;
     }
-    
+
 //**********************Challenges************************* 
-    /** returns true if the number represents a challenge
+
+    /**
+     * returns true if the number represents a challenge
+     *
      * @param num is the  number of the challenge
      * @return true if the  number represents a challenge
      **/
-     public boolean isChallenge(int num)
-     {
-         return (false);
-     }    
-   
-    /** Provides a String representation of an challenge given by 
+    public boolean isChallenge(int num) {
+        return (false);
+    }
+
+    /**
+     * Provides a String representation of an challenge given by
      * the challenge number
+     *
      * @param num the number of the challenge
-     * @return returns a String representation of a challenge given by 
+     * @return returns a String representation of a challenge given by
      * the challenge number
      **/
-    public String getChallenge(int num)
-    {
-        
-        
+    public String getChallenge(int num) {
+
+
         return "\nNo such challenge";
     }
-    
-    /** Provides a String representation of all challenges 
+
+    /**
+     * Provides a String representation of all challenges
+     *
      * @return returns a String representation of all challenges
      **/
-    public String getAllChallenges()
-    {
+    public String getAllChallenges() {
         String s = "\n************ All Challenges ************\n";
-       
+
         return s;
     }
-    
-    
-       /** Retrieves the challenge represented by the challenge 
-     * number.Finds a champion from the team who can meet the 
-     * challenge. The results of meeting a challenge will be 
-     * one of the following:  
-     * 0 - challenge won by champion, add reward to the treasury, 
+
+
+    /**
+     * Retrieves the challenge represented by the challenge
+     * number.Finds a champion from the team who can meet the
+     * challenge. The results of meeting a challenge will be
+     * one of the following:
+     * 0 - challenge won by champion, add reward to the treasury,
      * 1 - challenge lost on skills  - deduct reward from
      * treasury and record champion as "disqualified"
      * 2 - challenge lost as no suitable champion is  available, deduct
-     * the reward from treasury 
-     * 3 - If a challenge is lost and vizier completely defeated (no money and 
-     * no champions to withdraw) 
-     * -1 - no such challenge 
+     * the reward from treasury
+     * 3 - If a challenge is lost and vizier completely defeated (no money and
+     * no champions to withdraw)
+     * -1 - no such challenge
+     *
      * @param chalNo is the number of the challenge
      * @return an int showing the result(as above) of fighting the challenge
-     */ 
-    public int meetChallenge(int chalNo)
-    {
+     */
+    public int meetChallenge(int chalNo) {
         //Nothing said about accepting challenges when bust
-        int outcome = -1 ;
-        
+        int outcome = -1;
+
         return outcome;
     }
- 
+
 
     //****************** private methods for Task 3 functionality*******************
     //*******************************************************************************
-
-
-    private void setupChampions()
-    {
+    private void setupChampions() {
         championReserves = new ArrayList<>();
         championReserves.add(new Wizard("Ganfrank", 7, "transmutation", true));
         championReserves.add(new Wizard("Rudolf", 6, "invisibility", true));
@@ -258,14 +284,13 @@ public class Tournament implements CARE
         championReserves.add(new Warrior("Atlanta", 500, "bow"));
         championReserves.add(new Wizard("Krypton", 8, "fireballs", false));
         championReserves.add(new Wizard("Hedwig", 1, "flying", true));
-   }
-     
-    private void setupChallenges()
-    {
+    }
+
+    private void setupChallenges() {
 
 
     }
-        
+
     // Possible useful private methods
 //     private Challenge getAChallenge(int no)
 //     {
@@ -281,41 +306,41 @@ public class Tournament implements CARE
 
     //*******************************************************************************
     //*******************************************************************************
-  
-    /************************ Task 3.5 ************************************************/  
-    
+
+    /************************ Task 3.5 ************************************************/
+
     // ***************   file write/read  *********************
+
     /**
      * reads challenges from a comma-separated textfile and stores in the game
+     *
      * @param filename of the comma-separated textfile storing information about challenges
      */
-    public void readChallenges(String filename)
-    { 
-        
-    }   
-    
-     /** reads all information about the game from the specified file 
+    public void readChallenges(String filename) {
+
+    }
+
+    /**
+     * reads all information about the game from the specified file
      * and returns a CARE reference to a Tournament object, or null
+     *
      * @param fname name of file storing the game
      * @return the game (as a Tournament object)
      */
-    public Tournament loadGame(String fname)
-    {   // uses object serialisation 
-       Tournament yyy = null;
-       
-       return yyy;
-   } 
-   
-   /** Writes whole game to the specified file
+    public Tournament loadGame(String fname) {   // uses object serialisation
+        Tournament yyy = null;
+
+        return yyy;
+    }
+
+    /**
+     * Writes whole game to the specified file
+     *
      * @param fname name of file storing requests
      */
-   public void saveGame(String fname){
+    public void saveGame(String fname) {
         // uses object serialisation 
-        
+
+
     }
- 
-
 }
-
-
-
